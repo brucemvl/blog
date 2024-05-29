d36cf56ea27ac8b8bfeb05941670f3def31be062
bruce <bruce.monnerville@hotmail.fr>
2024-05-30 00:14:24 +0200

    mise a jour galerie

---
+++ jade/jade.js
@@ -0,0 +1,103 @@
+let grandeImg = document.querySelector(".grandeimg")
+let mini = document.querySelector(".miniatures")
+
+let photos = [  "Jade1.jpg",
+				"Jade2.jpg",
+				"Jade3.jpg",
+				"Jade4.jpg",
+				"Jade5.jpg",
+				"Jade6.jpg",
+				"Jade7.jpg",
+				"Jade8.jpg",
+				"Jade10.jpg",
+                "Jade11.jpg",
+                "Jade12.jpg",
+                "Jade13.jpg",
+                "Jade14.jpg",
+                "Jade15.jpg",
+                "Jade16.jpg",
+                "Jade17.jpg",
+                "Jade18.jpg",
+                "Jade19.jpg",
+                "Jade20.jpg",
+                "Jade21.jpg",
+                "Jade22.jpg",
+                "Jade23.jpg",
+                "Jade24.jpg",
+                "Jade25.jpg",
+                "Jade26.jpg",
+                "Jade27.jpg",
+                "Jade28.jpg",
+                "Jade29.jpg",
+                "Jade30.jpg",
+                "Jade31.jpg",
+                "Jade32.jpg",
+                "Jade33.jpg",
+                "Jade34.jpg",
+                "Jade35.jpg",
+                "Jade36.jpg",
+                "Jade37.jpg",
+                "Jade38.jpg",
+                "Jade39.jpg",
+                "Jade40.jpg",
+			]
+
+
+
+function genererImage(){
+
+    
+    for(let i=0; i<photos.length; i++){
+	   
+    
+    let miniatures = document.createElement("img")
+    let next = document.getElementById("next")
+    let prev = document.getElementById("prev")
+	miniatures.setAttribute("src", photos[i])
+	mini.appendChild(miniatures)
+
+
+    const imageElement = document.createElement("img")
+    imageElement.src = photos[i]
+    
+    	miniatures.addEventListener("click", e=>{
+            
+		console.log("jai clique sur la" + " "+ photos[i])
+		grandeImg.innerHTML = ""
+		grandeImg.appendChild(imageElement)
+        
+        next.addEventListener("click", ()=>{
+            imageElement.src = photos[i++]
+            if(photos[i] === undefined){
+                photos[i] = photos[0]
+            }
+            else{}
+        })
+
+        prev.addEventListener("click", ()=>{
+            imageElement.src = photos[i--]
+            if(photos[i] === undefined){
+                photos[i] = photos[38]
+            }
+            else{}
+        })
+        
+        
+        
+         })
+
+
+        
+
+        
+
+
+   }
+   
+  
+  
+   }
+   
+   
+   genererImage()
+   
