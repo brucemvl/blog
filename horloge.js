d723d8e9fa9c41b3de0a25cb9b88f44a11c7c40d
bruce <bruce.monnerville@hotmail.fr>
2024-08-07 04:41:17 +0200

    banniere dynamique

--- horloge.js
+++ horloge.js
@@ -6,3 +6,37 @@ function horloge(){
 var dateH = heure.getHours() + ":" + heure.getMinutes() + ":" + heure.getSeconds();
 document.querySelector(".heure").innerHTML = dateH;
           setTimeout(horloge, 1000);}
+
+          //photo aleatoire banniere
+		let banniere = document.querySelector(".banniere")
+		let photosBan = [
+			"mada.jpg",
+			"mada2.jpg",
+			"mada3.jpg",
+			"mada4.jpg",
+			"mada5.jpg",
+			"mada6.jpg",
+			"mada7.jpg"
+		]
+
+		function aleatoire(max) {
+			return Math.floor(Math.random() * max)
+		}
+		
+		//banniere.style.backgroundImage = `url(${randomPhoto})`
+
+		function change() {
+			let random = aleatoire(photosBan.length)
+		let randomPhoto = photosBan[random]
+			banniere.style.backgroundImage = `url(${randomPhoto})`
+			setTimeout(change, 7000)
+			banniere.style.transitionDuration = "2000ms"
+
+
+		}
+
+
+        function init(){
+            horloge()
+            change()
+        }
